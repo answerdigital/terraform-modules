c4759d0df82ab2cc63548d2b9dec60dfa92abcc2
Robert Gaskin <106234256+robg-answer@users.noreply.github.com>
2023-02-14 11:17:36 +0000

    Update Terraform_modules/rds_serverless_cluster/variables.tf
    Co-authored-by: Chris Buckley <chris@cmbuckley.co.uk>

--- Terraform_modules/rds_serverless_cluster/variables.tf
+++ Terraform_modules/rds_serverless_cluster/variables.tf
@@ -57,7 +57,7 @@ variable "backup_retention_period" {
   description = "This sets the retention period for backups, note: 1 day is a short time, but this has cost implications if higher"
   validation {
     condition     = var.backup_retention_period <= 35
-    error_message = "The maximum retention period that can be set is 35"
+    error_message = "The maximum retention period that can be set is 35."
   }
 
 }
