7e635314391f9cc96f9cf61659315b9646f9a3ed
Robert Gaskin <robert.gaskin@answerdigital.com>
2023-03-27 17:36:31 +0100

    Added secrets

--- modules/aws/vpc/test/main.tf
+++ modules/aws/vpc/test/main.tf
@@ -1,6 +1,13 @@
-module "vpc_subnet" {
+module "vpc_subnet_basic" {
   source               = "./.."
   owner                = "joe_blogs"
-  project_name         = "example_project_name"
+  project_name         = "example_vpc_with_logs"
   enable_vpc_flow_logs = true
+}
+
+module "vpc_subnet_no_vpc_logs" {
+  source               = "./.."
+  owner                = "joe_blogs"
+  project_name         = "example_vpc_no_logs"
+  enable_vpc_flow_logs = false
 }
--- modules/aws/vpc/test/output.tf
+++ modules/aws/vpc/test/output.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-  value       = module.vpc_subnet.vpc_id
+  value       = module.vpc_subnet_basic.vpc_id
 }
 
 output "public_subnet_ids" {
-  value       = module.vpc_subnet.public_subnet_ids
+  value       = module.vpc_subnet_basic.public_subnet_ids
   description = "A list of the public subnet IDs that have been created. This output is of type `list(string)`."
 }
 
 output "private_subnet_ids" {
-  value       = module.vpc_subnet.private_subnet_ids
+  value       = module.vpc_subnet_basic.private_subnet_ids
   description = "A list of the private subnet IDs that have been created. This output is of type `list(string)`."
 }
 
 output "az_zones" {
-  value       = module.vpc_subnet.az_zones
+  value       = module.vpc_subnet_basic.az_zones
   description = "A list of the Availability Zones that have been used. This output is of type `string`."
 }
 
