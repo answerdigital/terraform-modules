45b436109768789ed846605f9ea9b85008a80ec6
Robert Gaskin <robert.gaskin@answerdigital.com>
2023-05-18 14:53:22 +0100

    Typo in public ip description

--- modules/aws/ec2/output.tf
+++ modules/aws/ec2/output.tf
@@ -1,6 +1,6 @@
 output "instance_public_ip_address" {
   value       = aws_instance.ec2.public_ip
-  description = "This outputs the public IP associated with the EC2 instance. Note that this ouput will be the same as the elastic IP if `needs_elastic_ip` is set to `true`. This output is of type `string`."
+  description = "This outputs the public IP associated with the EC2 instance. Note that this output will be the same as the elastic IP if `needs_elastic_ip` is set to `true`. This output is of type `string`."
 }
 
 output "instance_id" {
