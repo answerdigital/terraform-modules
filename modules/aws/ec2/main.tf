245fb3e70a8ddca67870827e9e3202d32b6af397
Robert Gaskin <robert.gaskin@answerdigital.com>
2023-05-26 15:20:17 +0100

    use >= for versioning

--- modules/aws/ec2/main.tf
+++ modules/aws/ec2/main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = ">= 4.0.4"
     }
     aws = {
       source = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.0"
     }
   }
 }
