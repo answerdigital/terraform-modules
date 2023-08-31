c2e8353f402964fdea3a7059f04901ef5d3ee888
Kris Bloe <kris.bloe@answerdigital.com>
2023-08-31 11:55:52 +0100

    Just formatting

--- modules/aws/vpc/variables.tf
+++ modules/aws/vpc/variables.tf
@@ -72,7 +72,7 @@ variable "enable_dns_hostnames" {
 # Availability Zone variables
 variable "azs" {
   type        = list(string)
-  description = "This is a list that specifies all the Availability Zones that will have public and private subnets in it. Defaulting this value to an empty list selects of all the Availability Zones in the region you specify when defining the provider in your terraform project."
+  description = "This is a list that specifies all the Availability Zones that will have public and private subnets in it. Defaulting this value to an empty list selects of all the Availability Zones in the region you specify when defining the provider in your Terraform project."
   default     = []
 }
 
