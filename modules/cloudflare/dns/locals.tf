69f2a95d34796c3d02126988024693ffcaec8a9e
Chris Buckley <chris@cmbuckley.co.uk>
2024-12-06 15:43:14 +0000

    Fix the zone_id output when not creating the zone internally

--- modules/cloudflare/dns/locals.tf
+++ modules/cloudflare/dns/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  zone_id = var.create_zone ? cloudflare_zone.dns[0].id : data.cloudflare_zones.lookup[0].id
+  zone_id = var.create_zone ? cloudflare_zone.dns[0].id : data.cloudflare_zones.lookup[0].zones[0].id
 
   security_contact = var.security_contact != null ? var.security_contact : format("security@%s", var.domain)
 }
