0458f812db341d93b1f260201dd939403ef9bb51
Chris Buckley <chris@cmbuckley.co.uk>
2025-01-29 20:38:54 +0000

    Cloudflare provider minimum version
    Version 5.0.0 is a major rewrite of the provider. Migration guide here:
    https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/guides/version-5-upgrade

--- modules/cloudflare/dns/locals.tf
+++ modules/cloudflare/dns/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  zone_id = var.create_zone ? cloudflare_zone.dns[var.domain].id : data.cloudflare_zones.lookup[var.domain].zones[0].id
+  zone_id = var.create_zone ? cloudflare_zone.dns[var.domain].id : data.cloudflare_zones.lookup[var.domain].result[0].id
 
   security_contact = var.security_contact != null ? var.security_contact : format("security@%s", var.domain)
 }
